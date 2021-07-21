cd84d85a23fb45b4f578ae9e0d241448bfac90b4
Lucas Matheus Nunes Pereira <86432484+Lukiticas@users.noreply.github.com>
2021-07-21 15:03:00 -0300

    Update jogo_forca.py

--- jogos/jogo_forca.py
+++ jogos/jogo_forca.py
@@ -1,10 +1,30 @@
-def jogar_fc():
     print("***********************")
-    print("bem vindo a FORCA!!")
-    print("***********************", "\n")
+def jogo_fc():
+    print('*****************************************')
+    print('****** Bem-vindo ao jogo da Forca *******')
+    print('*****************************************')
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
-    print("fim do jogo!")
+    enforcou = False
+    acertou = False
 
-if __name__ == "__main__":
-    jogar_fc()
+    while (not acertou and not enforcou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+                print(letras_acertadas) 
+            index += 1
+
+        print("Jogando...")
+
+    print("Fim do jogo")
+
+if(__name__ == '__main__'):
+    jogo_fc()
