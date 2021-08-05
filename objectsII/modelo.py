6cbf2d006923d092872be7f28d815b519656d8c3
Lucas Matheus Nunes Pereira <86432484+Lukiticas@users.noreply.github.com>
2021-08-05 15:23:08 -0300

    projeto

--- objectsII/modelo.py
+++ objectsII/modelo.py
@@ -1,70 +1,47 @@
 from typing import get_args
 
-
-class filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__duracao = duracao
-        self.__likes = 0
+class programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
     
     def like(self):
-        self.__likes += 1
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome  
+        return self._nome  
     
     @property
     def ano(self):
-        return self.__ano
+        return self._ano
+    
+    @property
+    def likes(self):
+        return self._likes
+    
+    @nome.setter 
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title() 
 
+class filme(programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self._duracao = duracao
+    
     @property
     def duracao(self):
-        return self.__duracao
-    
-    @property
-    def likes(self):
-        return self.__likes
-    
-    @nome.setter 
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title() 
-    
+        return self._duracao
 
-
-
-class serie: 
+class serie(programa): 
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__temporadas = temporadas
-        self.__likes = 0
+        super().__init__(nome, ano)
+        self._temporadas = temporadas
         
-    def like(self):
-        self.__likes += 1
-    
-    @property
-    def nome(self):
-        return self.__nome  
-    
-    @property
-    def ano(self):
-        return self.__ano
-
     @property
     def temporadas(self):
-        return self.__temporadas
-    
-    @property
-    def likes(self):
-        return self.__likes
-    
-    @nome.setter 
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title() 
-    
-    
+        return self._temporadas
 
 
 vingadores = filme("Vingadores - guerra infinita", 2018, 160)
